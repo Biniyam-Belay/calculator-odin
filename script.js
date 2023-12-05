90dfeade59d9902fe9ea1a16ba5231576f6d2b25
Biniyam Belay <binibelaygom@gmail.com>
2023-12-06 01:25:10 +0300

    edited the js file, to implement the addition operation.

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-const dsp = document.querySelector(".display2")
+scr = [];
+b = [];
 
-let one = document.getElementById("1");
-one.addEventListener('click', () => {
-    console.log("One")
-    dsp.textContent = '1'
-})
+const dsp = document.querySelector(".display2");
+const numkey = document.querySelectorAll(".num-key");
+
+
+// A code that takes the input from nember keys and displays them on the screen
+numkey.forEach(function (numkey) {
+  numkey.addEventListener("click", () => {
+    scr.push(numkey.innerHTML);
+    const scrc = parseInt(scr.join(""), 10);
+    dsp.textContent = scrc;
+  });
+});
+
+// function that adds two number a and b
+function add(a, b) {
+  return a + b;
+}
+
+// a code that runs the addition operation
+// here make it store the value of b after storing the previous input as value of A
+const addd = document.getElementById("add");
+addd.addEventListener("click", () => {
+  a = parseInt(scr.join(""), 10);
+  scr = []
+  b = parseInt(scr.join(""), 10);
+  dsp.textContent = ''
+  console.log(a);
+  console.log(b)
+  
+});
+
+const eql = document.getElementById("equals");
+eql.addEventListener("click", () => {
+  dsp.textContent = add(a, b);
+});
+
+const clr = document.getElementById('clear');
+clr.addEventListener("click", () => {
+    dsp.textContent = ''
+    scr = []
+    b = []
+  });
